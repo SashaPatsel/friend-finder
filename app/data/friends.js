14b36e63aac1c94d11f6a75ebd5cef17c0e210ab
Sasha Patsel <sashapatsel@sashas-mbp.attlocal.net>
2018-02-28 20:39:46 -0800

    added questions and modals

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@ var friends = [
 
 {
   "name":"Bear",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "photo":"../../images/bear.jpg",
   "scores":[
       5,
       1,
@@ -17,139 +17,141 @@ var friends = [
     ]
 }, {
     "name":"Tiger",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "photo":"../../images/tiger.jpeg",
   "scores":[
-      5,
-      1,
+      2,
       4,
-      4,
-      5,
+      3,
       1,
       2,
       5,
-      4,
-      1
+      2,
+      1,
+      3,
+      5
     ]
 
 },{
   "name":"Dog",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "photo":"../../images/dog.jpeg",
   "scores":[
-      5,
       1,
-      4,
-      4,
       5,
+      3,
+      2,
+      4,
+      2,
+      3,
       1,
       2,
-      5,
-      4,
       1
     ]
 },{
   "name":"Cat",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "photo":"../../images/cat.jpeg",
   "scores":[
-      5,
-      1,
+      2,
       4,
-      4,
-      5,
-      1,
+      2,
       2,
       5,
-      4,
-      1
+      1,
+      1,
+      1,
+      5,
+      4
     ]
 },{
   "name":"Horse",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "photo":"../../images/horse.jpg",
   "scores":[
-      5,
-      1,
       4,
       4,
-      5,
-      1,
+      3,
       2,
-      5,
+      1,
+      3,
       4,
-      1
+      4,
+      5,
+      2
     ]
 },{
   "name":"Lion",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "photo":"../../images/lion.png",
   "scores":[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
+      3,
       2,
-      5,
       4,
-      1
+      5,
+      1,
+      1,
+      3,
+      4,
+      3,
+      3
     ]
 },{
   "name":"Eagle",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "photo":"../../images/eagle2.jpg",
   "scores":[
-      5,
-      1,
       4,
       4,
-      5,
-      1,
+      4,
+      4,
+      3,
       2,
+      1,
       5,
       4,
-      1
+      3
     ]
 },{
   "name":"Shark",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "photo":"../../images/shark.jpg",
   "scores":[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
       2,
+      2,
+      2,
+      2,
+      3,
+      3,
+      3,
       5,
-      4,
-      1
+      5,
+      2
     ]
 },{
   "name":"Giraffe",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "photo":"../../images/giraffe.jpg",
   "scores":[
       5,
-      1,
-      4,
-      4,
       5,
-      1,
-      2,
       5,
-      4,
-      1
+      5,
+      5,
+      5,
+      5,
+      5,
+      5,
+      5
     ]
 },{
   "name":"Weasel",
-  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  "photo":"../../images/weasel.jpg",
   "scores":[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
       2,
-      5,
+      2,
       4,
+      2,
+      1,
+      3,
+      4,
+      2,
+      1,
       1
     ]
 },
 ]
+
+module.exports = friends
--- server.js
+++ server.js
@@ -5,12 +5,11 @@ var path = require("path");
 var app = express()
 var port = 1993
 
-app.post("/api/new", function(req, res) {
-  
-})
+
 
 // require("./app/routing/apiRoutes.js")
 // require("./app/routing/htmlRoutes.js")
+var friends = require("./app/data/friends.js")
 
 // apiroutes
   app.use(bodyParser.urlencoded({ extended: false }));
@@ -40,9 +39,39 @@ app.get("/survey", function(req, res) {
   res.sendFile(path.join(__dirname, "app/public", "survey.html"));
 });
 
+//Stays in server.js
+app.post("/api/new", function(req, res) {
+    var newMatch = req.body;
+
+  console.log(newMatch);
+
+friends.push(newMatch)
+
+  // res.json(newMatch);
 
 
+//   var matchName = ""
+//   var totalDifference = 40;
+//   var matchScores = newMatch.scores;
+//       for (var i = 0; i < friends.length; i++) {
+
+//       var diff = 0;
+//       for (var j = 0; j < matchScores.length; j++) {
+//         diff += Math.abs(friends[i].scores[j] - matchScores[j]);
+//       }
+
+//       if (diff < totalDifference) {
 
 
+//         totalDifference = diff;
+//         matchName = friends[i].name;
+//         // matchImage = friends[i].photo;
+//       }
+//     }
+//     res.json({matchName: matchName});
+// console.log(res)
+// console.log(res.json({matchName: matchName}))
+// console.log(matchName)
+})
 
 app.listen(port);
